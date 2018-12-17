874377b57b1de433c58dba30b8d6da637b08020f
John Kawahara <kawaharajohn@gmail.com>
2018-12-16 18:23:10 -0800

    Add survey data, friends data (friends.js); add images

---
+++ app/data/friends.js
@@ -0,0 +1,177 @@
+// Survey data array with following properties: questions
+var survey = [
+  'You are motivated to get out of bed before sunrise',
+  'You think formal training is important',
+  'You appreciate bizare or quirky talents over professional skills',
+  'You believe it\'s important to have role models',
+  'You prefer to listen instead of talking when in social situations',
+  'You routinely go out at night',
+  'You enjoy maintaining a comfortable home environment',
+  'You are chased by others more often than being the one chasing others',
+  'You take the path least traveled',
+  'You drink a lot'
+];
+
+// Friends data object with following properties: name, photo, scores
+var friends = [
+  {
+    name: 'Labrador',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Hummingbird',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Cat',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Goat',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Owl',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Skunk',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Squirrel',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Wolf',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Dolphin',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+  {
+    name: 'Penguin',
+    photo: '',
+    scores: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      
+    ]
+  },
+]
